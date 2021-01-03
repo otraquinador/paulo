f38b3c8dafae88814d1fbf8a844cb665733fbbff
otraquinador <76921835+otraquinador@users.noreply.github.com>
2021-01-03 20:39:53 -0300

    Add files via upload

---
+++ menu1.js
@@ -0,0 +1,26 @@
+exportações . menu1  =  ( id ,  A187 ,  tampilTanggal ,  whatsapp ,  youtube ,  tampilWaktu ,  instagram ,  nomer ,  aktif )  =>  {
+	return  `
+❉───────────❉
+ Pᴀᴜʟɪɴ DOMINA 🤨
+❉───────────❉
+  Pᴀᴜʟɪɴ DOMINA😁 Dᴀ́ UM BIZU AI EM BAIXO POHA
+╔════════════════════
+║ COMANDOS:
+╠════════════════════
+║╭──❉ * MIDIA * ❉──
+║│➸ _ *! Adesivo * _
+║│➸ _ *! Foto de menina * _
+║│➸ _ *! Foto de menino * _
+║│➸ _ *! Pokémon * _ 
+║│➸ _ *! Loli * _
+║│➸ _ *! Fotoanime * _
+║│➸ _ *! Waifu * _
+║│➸ _ *! Ttp * _ <texto>
+║│➸ _ *! Meme * _
+║│➸ _ *! Ssweb * _ <link>
+║╰────────────
+╠════════════════════
+║  Pᴀᴜʟɪɴ DOMINA
+║ wa.me/+551999313301
+╚════════════════════`
+}
---
+++ menu2.js
@@ -0,0 +1,33 @@
+exportações . menu2  =  ( id ,  A187 ,  tampilTanggal ,  whatsapp ,  youtube ,  tampilWaktu ,  instagram ,  nomer ,  aktif )  =>  {
+	return  `
+❉───────────❉
+ Pᴀᴜʟɪɴ  
+❉───────────❉
+ Pᴀᴜʟɪɴ DOMINA 👇👇
+	
+╔════════════════════
+║ * MENU2 *    
+╠════════════════════
+║╭──❉ * BAIXAR * ❉──
+║│➸ _ *! Ytmp3 * _ <link>
+║│➸ _ *! Ytmp4 * _ <link>
+║│➸ _ *! Fb * _ <link>
+║│➸ _ *! Tiktok * _ <link>
+║│➸ _ *! Twt * _ <link>
+║│➸ _ *! Ig * _ <link>
+║╰───────────
+║ * BOT $ { A187 } *
+╠════════════════════
+║╭──❉ * 18 + * ❉──
+║│➸ _ *! Indohot * _
+║│➸ _ *! Cersex * _
+║│➸ _ *! Hentai * _
+║│➸ _ *! Indohot * _
+║│➸ _ *! Nekonime * _
+║╰────────────
+╠════════════════════
+║  Pᴀᴜʟɪɴ DOMINA
+╚════════════════════`
+}
+
+
